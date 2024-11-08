475db5f69bb46d7e24b6513097b44d4df47bb05e
Valentyn Mostovyi <mostovoy.v@gmail.com>
2024-11-08 21:34:11 +0100

    Finish module 2

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,6 +1,7 @@
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import s from "./FeedbackForm.module.css"
 import { useId } from 'react';
+import * as Yup from "yup";
 
 // Об'єкт початкових значень полів форми
 const initialValues = {
@@ -9,6 +10,13 @@ const initialValues = {
 	message: "",
 	level: "good",
 }
+
+const FeedbackSchema = Yup.object().shape({
+	username: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+	email: Yup.string().email("Must be a valid email!").required("Required"),
+	message: Yup.string().min(3, "Too short").max(256, "Too long").required("Required"),
+	level: Yup.string().oneOf(["good", "neutral", "bad"]).required("Required"),
+});
 
 const FeedbackForm = () => {
 
@@ -23,23 +31,35 @@ const FeedbackForm = () => {
 	};
 
 	return (
-		<Formik initialValues={initialValues} onSubmit={handleSubmit}>
+		<Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
 			<Form className={s.form}>
-				<label htmlFor={nameFieldId}>Username</label>
-				<Field className={s.input} type="text" name="username" id={nameFieldId} />
+				<div className={s.wrap}>
+					<label htmlFor={nameFieldId}>Username</label>
+					<Field className={s.input} type="text" name="username" id={nameFieldId} />
+					<ErrorMessage className={s.error} name="username" component="span" />
+				</div>
 
-				<label htmlFor={emailFieldId}>Email</label>
-				<Field className={s.input} type="email" name="email" id={emailFieldId} />
+				<div className={s.wrap}>
+					<label htmlFor={emailFieldId}>Email</label>
+					<Field className={s.input} type="email" name="email" id={emailFieldId} />
+					<ErrorMessage className={s.error} name="email" component="span" />
+				</div>
 
-				<label htmlFor={msgFieldId}>Message</label>
-				<Field className={s.input} as="textarea" name="message" id={msgFieldId} rows="5" />
+				<div className={s.wrap}>
+					<label htmlFor={msgFieldId}>Message</label>
+					<Field className={s.input} as="textarea" name="message" id={msgFieldId} rows="5" />
+					<ErrorMessage className={s.error} name="message" component="span" />
+				</div>
 
-				<label htmlFor={levelFieldId}>Service satisfaction level</label>
-				<Field className={s.input} as="select" name="level" id={levelFieldId}>
-					<option value="good">Good</option>
-					<option value="neutral">Neutral</option>
-					<option value="bad">Bad</option>
-				</Field>
+				<div className={s.wrap}>
+					<label htmlFor={levelFieldId}>Service satisfaction level</label>
+					<Field className={s.input} as="select" name="level" id={levelFieldId}>
+						<option value="good">Good</option>
+						<option value="neutral">Neutral</option>
+						<option value="bad">Bad</option>
+					</Field>
+					<ErrorMessage className={s.error} name="message" component="span" />
+				</div>
 
 				<button className={s.button} type="submit">Submit</button>
 			</Form>
